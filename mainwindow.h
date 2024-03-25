9082ee287d9693532f957c09ba81fb44cfea0e4e
ks <2671472814@qq.com>
2024-03-25 15:54:05 +0800

    稍微搭建一个框，好像有一点问题

---
+++ mainwindow.h
@@ -0,0 +1,24 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class CMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    CMainWindow(QWidget *parent = nullptr);
+
+    ~CMainWindow();
+
+    void run_client();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
