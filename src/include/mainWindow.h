4958b35cd8b1a136b6d2071e67f0346502408aad
ks <2671472814@qq.com>
2024-03-29 14:37:44 +0800

    增加定时发送心跳和接收的线程，接收到心跳返回包后将最新的数据更新到对应对象中，然后修改对应的显示，除显示，其他测试无误。

--- src/include/mainWindow.h
+++ src/include/mainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <qDebug>
 
 #include "clientQQ.h"
 
@@ -18,11 +19,32 @@ public:
 
     ~CMainWindow();
 
+    //初始化客户端，并启动客户端中接收指令字节流的线程
     int recv_thread_init();
+
+    //启动处理接收到的指令的线程(处理对实时性要求不高的,如心跳包的好友申请和新好友,好友未读消息))
+    void deal_cmd_thread_init();
+    //处理的具体执行
+    void thread_deal_recv_cmd();
+
+    /*
+     * 初始化用户好友相关信息(好友信息，好友申请，好友未读信息)
+     */
+    int user_friends_init();
 
 private:
     Ui::MainWindow *ui;
 
     std::shared_ptr<ClientQQ> _mainClientPtr;
+
+
+    //当前用户
+    CUser _currentUser;
+    //好友列表
+    std::vector<CUser> _friendLists;
+    //好友请求列表
+    std::vector<CUser> _requestUserLists;
+    //未读消息列表
+    std::vector<CMsg> _notRecvMsgsLists;
 };
 #endif // MAINWINDOW_H
--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -28,6 +28,15 @@ CMainWindow::CMainWindow(QWidget *parent)
     loginDig.exec();
     std::shared_ptr<CLoginCmd> loginCmdPtr=loginDig.get_login_cmd_ptr();
     loginCmdPtr->show_do_command_info();
+    _currentUser=loginCmdPtr->get_login_user();
+    _friendLists=loginCmdPtr->get_friend_lists();
+    _notRecvMsgsLists=loginCmdPtr->get_not_recv_msg_lists();
+
+    //启动心跳发送线程
+    _mainClientPtr->heart_thread_init(_currentUser);
+
+    //启动指令处理线程(将接收到的指令数据更新到对应对象中)
+    deal_cmd_thread_init();
 }
 
 int CMainWindow::recv_thread_init()
@@ -44,6 +53,61 @@ int CMainWindow::recv_thread_init()
 
     std::cout<<"recv thread is runing"<<std::endl;
 }
+
+void CMainWindow::deal_cmd_thread_init()
+{
+    std::thread dealCmdThread(&CMainWindow::thread_deal_recv_cmd,this);
+    dealCmdThread.detach();
+}
+void CMainWindow::thread_deal_recv_cmd()
+{
+    std::list<std::shared_ptr<CmdBase>> &cmdPtrLists=_mainClientPtr->get_cmd_ptr_lists();
+    std::list<std::shared_ptr<CmdBase>>::iterator itNow;
+    std::vector<std::list<std::shared_ptr<CmdBase>>::iterator> delIteratorLists;
+    std::vector<std::list<std::shared_ptr<CmdBase>>::iterator>::iterator itDel;
+    std::shared_ptr<CHeartRequestCmd> heartCmdPtr;
+    //5秒处理一次
+    //只处理heartRequestCmd和dataMsgCmd
+    //用户信息修改，需要等待是否修改成功，所以放在这里需要等比较久
+    while(1)
+    {
+        Sleep(1000);
+        //这里需要上一个读锁
+        for( itNow=cmdPtrLists.begin();itNow!=cmdPtrLists.end();itNow++)
+        {
+            if((*itNow)->_childCmdType==CmdBase::HEART_CMD)
+            {
+                if((*itNow)->_childDoCommandReturn==false)
+                {
+                    qDebug()<<"[E]  heart cmd return a error, pleace check";
+                }
+                qDebug()<<"[I] get a new heart cmd";
+                heartCmdPtr =std::dynamic_pointer_cast<CHeartRequestCmd>(*(itNow));
+                _friendLists=heartCmdPtr->get_friend_lists();
+                _requestUserLists=heartCmdPtr->get_friendship_request_lists();
+                delIteratorLists.push_back(itNow);
+            }
+//            else if((*itNow)->_childCmdType==CmdBase::DTAT_MSG_CMD)
+//            {
+//                if((*itNow)->_childDoCommandReturn==false)
+//                {
+//                    QMessageBox::critical(this,_Q_U("错误"),_Q_U("账号密码错误"));
+//                }
+//                break;
+//            }
+        }
+        //这里加一个写锁，删除list中的数据
+        for(itDel=delIteratorLists.begin();itDel!=delIteratorLists.end();itDel++)
+        {
+            cmdPtrLists.erase(*itDel);
+        }
+        //清空需删除数据
+        delIteratorLists.clear();
+        itDel=delIteratorLists.begin();
+        Sleep(3000);
+    }
+}
+
 CMainWindow::~CMainWindow()
 {
     delete ui;
